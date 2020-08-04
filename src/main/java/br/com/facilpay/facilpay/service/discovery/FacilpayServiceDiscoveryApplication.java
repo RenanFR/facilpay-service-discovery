a90ef20ccb7522319d7043efe65605b1a9c98ebb
Renan Rodrigues <renanfr1047@gmail.com>
2020-08-04 17:29:54 -0300

    first commit

---
+++ src/main/java/br/com/facilpay/facilpay/service/discovery/FacilpayServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package br.com.facilpay.facilpay.service.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class FacilpayServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilpayServiceDiscoveryApplication.class, args);
+	}
+
+}
